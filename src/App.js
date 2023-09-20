9b29c90371136cefa3f3647d94b9d1f41576b611
BK <39931731+BitsBytesandLife@users.noreply.github.com>
2023-09-20 19:25:08 -0400

    Add Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  const Person = (props) => {
+    return (
+      <>
+        <h1>Name: {props.name}</h1>
+        <h2>Last: {props.lastname}</h2>
+        <h2>Age: {props.age}</h2>
+      </>
+    )
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hey BK 
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name='Brian' lastname = 'King' Age='42' />
+      <Person name='Peter' lastname = 'Parker' Age='16'/>
+      <Person name='Scott' lastname = 'Summer' Age='40'/>
+      <Person name='Reed'  lastname = 'Richards'Age='45'/>
+      <Person name='Bruce' lastname = 'Banner'  Age='43'/>
     </div>
   );
 }
