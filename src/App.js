05c5f49f3aab9880d4bc54e7b067a51f655c2e76
BK <39931731+BitsBytesandLife@users.noreply.github.com>
2023-09-23 17:22:21 -0400

    add counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  
-  const Person = (props) => {
-    return (
-      <>
-        <h1>Name: {props.name}</h1>
-        <h2>Last: {props.lastname}</h2>
-        <h2>Age: {props.age}</h2>
-      </>
-    )
-  }
+  const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+      alert("You've changed the counter to " + counter)
+  }, [counter]);
+ 
   return (
     
     <div className="App">
-      <Person name='Brian' lastname = 'King' Age='42' />
-      <Person name='Peter' lastname = 'Parker' Age='16'/>
-      <Person name='Scott' lastname = 'Summer' Age='40'/>
-      <Person name='Reed'  lastname = 'Richards'Age='45'/>
-      <Person name='Bruce' lastname = 'Banner'  Age='43'/>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
